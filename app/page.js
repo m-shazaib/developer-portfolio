b4d69cbe15d8785ae19058bd70868bddfceaf94d
m-shazaib <shahzaib3742@gmail.com>
2024-07-26 23:31:29 +0500

    `chore: Add @vercel/analytics for page tracking`

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { Analytics } from "@vercel/analytics/react"
 
 export default async function Home() {
   return (
@@ -17,6 +18,7 @@ export default async function Home() {
       <Projects />
       <Education />
       <ContactSection />
+      <Analytics />
     </>
   );
 }
