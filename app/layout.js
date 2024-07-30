92514cea0c103ba5d745185c6bf50c55022ca773
m-shazaib <shahzaib3742@gmail.com>
2024-07-30 08:18:57 +0500

    `chore: Update meta description and page title in layout.js`

--- app/layout.js
+++ app/layout.js
@@ -17,23 +17,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="0OOQ1LA27Ro5drsl81u-P1O19KquM-w-3yIt0UwN0zg" />
-        <meta name="description" content="Discover Muhammad Shahzaib: Frontend Developer with a Talent for New Technologies and solving problems. Open to collaboration and new challenges." />
-        <meta name="keywords" content="Muhammad Shahzaib, Muhammad Shahzaib Tariq, Shahzaib, Islamabad, Software Developer, Full Stack Developer, JavaScript, React Native, MERN Stack, Web Development, Mobile Development" />
-        <meta name="author" content="Muhammad Shahzaib" />
-        <title>Muhammad Shahzaib: Portfolio - Software Developer</title>
-      </head>
-      <body className={inter.className}>
-        <ToastContainer />
-        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-          <Navbar />
-          {children}
-          <ScrollToTop />
-        </main>
-        <Footer />
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
-      </body>
-    </html>
+    <head>
+      <meta name="google-site-verification" content="0OOQ1LA27Ro5drsl81u-P1O19KquM-w-3yIt0UwN0zg" />
+      <meta name="description" content="Discover Muhammad Shahzaib LinkedIn: React Native Frontend Mobile Developer with a Talent for New Technologies and solving problems. Open to new opportunities and new challenges." />
+      <meta name="keywords" content="React Native, Mobile Developer, Muhammad Shahzaib, Shahzaib, Islamabad, Software Developer, Full Stack Developer, JavaScript, MERN Stack, Web Development, Mobile Development" />
+      <meta name="author" content="Muhammad Shahzaib" />
+      <link rel="canonical" href="https://mshahzaib.vercel.app/" />
+      <link rel="alternate" hreflang="en" href="https://mshahzaib.vercel.app/en" />
+      <link rel="alternate" hreflang="es" href="https://mshahzaib.vercel.app/es" />
+      <title>Muhammad Shahzaib: Portfolio - React Native Mobile Developer</title>
+    </head>
+    <body className={inter.className}>
+      <ToastContainer />
+      <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+        <Navbar />
+        {children}
+        <ScrollToTop />
+      </main>
+      <Footer />
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM} />
+    </body>
+  </html>
   );
 }
